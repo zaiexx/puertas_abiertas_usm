5cde1797023c864274013168d0d07f5e54b0f0fa
Manuel Pérez <manuel.perez@alumnos.usm.cl>
2017-10-20 13:57:27 -0300

    asistencias

--- app/Http/Controllers/AsistenciasController.php
+++ app/Http/Controllers/AsistenciasController.php
@@ -9,6 +9,12 @@ use App\Http\Requests;
 
 class AsistenciasController extends Controller
 {
+
+     public function __construct() {
+
+        $this->middleware('auth');
+    }
+
     /**
      * Display a listing of the resource.
      *
